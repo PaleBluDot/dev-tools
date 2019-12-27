6dc787099101187ded613951eaeec3af44515dc0
Pavel Sanchez <info@pavelsanchez.com>
2019-12-26 20:10:14 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# PS Dev Tools
+
+A series of dev tools that I'm building to help learn more JS.
+
+## Todo List
+
+- [ ] Move to 11ty (Hylia)
+- [ ] Remove Back-end
+- [ ] Add Gulp
+- [ ] Add Babel
+- [ ] Tools
+  - [x] ~~Character Counter~~
+  - [ ] Word Counter
+  - [ ] Letter Counter
+  - [ ] Calculator
+  - [ ] URL Parameter Generator
+  - [ ] HTML-to-Text Convertor
+  - [ ] Social Share Generator
---
+++ src/app.js
@@ -0,0 +1,53 @@
+const message = document.querySelector('#textinput');
+const form = document.querySelector('#textbox');
+const reset = document.querySelector('#reset-button');
+const countOutput = document.querySelector('#countOutput');
+const mobileMenu = document.querySelector('#mobile-menu');
+const menuOpen = document.querySelector('#menu-button');
+const menuClose = document.querySelector('#menu-close');
+
+/* ==============================
+@Menu
+============================== */
+// *MENU OPEN
+menuOpen.addEventListener('click', function(e) {
+	// PREVENT DEFAULT SUBMIT BEHAVIOR
+	e.preventDefault();
+	mobileMenu.style.transform = 'translateX(0px)';
+});
+
+// *MENU CLOSE
+menuClose.addEventListener('click', function(e) {
+	// PREVENT DEFAULT SUBMIT BEHAVIOR
+	e.preventDefault();
+	mobileMenu.style.transform = 'translateX(-500px)';
+});
+2;
+/* ==============================
+@Character Count
+============================== */
+// *PUT Character Counter IN BODY
+form.addEventListener('submit', function(e) {
+	// PREVENT DEFAULT SUBMIT BEHAVIOR
+	e.preventDefault();
+
+	// GET Character Counter
+	const value = message.value;
+	const count = value.length;
+
+	// DISPLAY COUNT IN BODY
+	countOutput.innerHTML = count;
+});
+
+// *RESET FORM TO 0
+reset.addEventListener('click', function(e) {
+	// PREVENT DEFAULT SUBMIT BEHAVIOR
+	e.preventDefault();
+	// CLEAR ON SUBMIT
+	countOutput.innerHTML = '0';
+	message.value = '';
+});
+
+/* ==============================
+@Word Count
+============================== */
